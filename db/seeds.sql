f376fe12544b7ccd1fce59c4bbd6fe73b7fc134e
stefan hall <stefhall@comcast.net>
2021-06-09 22:47:16 -0700

    adding to main

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butlers', 1),
+  ('Unica', 'Zurnn', 1);
